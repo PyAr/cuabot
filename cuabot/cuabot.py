13fd4921c0bca2ba2fc8f85e8e1c9d420804a097
FedeG <federicogonzalez761@gmail.com>
2019-11-22 21:02:02 -0300

    #1 - Add anonymous_question_handler

--- cuabot/cuabot.py
+++ cuabot/cuabot.py
@@ -39,6 +39,10 @@ class CUABot:
                     for index, room in enumerate(self.config['rooms'])]
         reply_markup = InlineKeyboardMarkup(keyboard)
         update.message.reply_text(self.config['select_room_message'], reply_markup=reply_markup)
+    
+    def anonymous_question_handler(self, update, context):
+        context.chat_data['anonymous'] = True
+        self.question_handler(update, context)
 
     def select_room_handler(self, update, context):
         query = update.callback_query
@@ -47,13 +51,17 @@ class CUABot:
 
     def question_text_handler(self, update, context):
         # Here we take out the room from the context to not send the question to other room chat
+        anonymous = context.chat_data.pop('anonymous', False)
         selected_room_raw = context.chat_data.pop('selected_room', None)
         if selected_room_raw is None:
             update.message.reply_text(self.config['no_room_selected_message'])
         else:
             room_index = int(selected_room_raw)
             selected_room = self.config['rooms'][room_index]
-            update.message.forward(selected_room['chat_id'])
+            if (anonymous):
+                context.bot.send_message(chat_id=selected_room['chat_id'], text=update.message.text)
+            else:
+                update.message.forward(selected_room['chat_id'])
             update.message.reply_text("{0} {1}".format(self.config['sent_question_message'],selected_room['name']))
 
     def run(self):
@@ -64,6 +72,7 @@ class CUABot:
         updater.dispatcher.add_handler(CommandHandler('start', self.start_handler))
         updater.dispatcher.add_handler(CommandHandler("get_chat_id", self.get_chat_id))
         updater.dispatcher.add_handler(CommandHandler(self.config['question_handler'], self.question_handler))
+        updater.dispatcher.add_handler(CommandHandler(self.config['anonymous_question_handler'], self.anonymous_question_handler, pass_chat_data=True))
         updater.dispatcher.add_handler(CallbackQueryHandler(self.select_room_handler, pass_chat_data=True))
         updater.dispatcher.add_handler(MessageHandler(Filters.text, self.question_text_handler, pass_chat_data=True))
         updater.dispatcher.add_error_handler(self.on_error_handler)
